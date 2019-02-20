033dee2912021ae79a581c027ee7e485cc1abab0
kraudo <luke@belinc.me>
2019-02-20 09:06:29 -0500

    Add files via upload

---
+++ 2018/day 1/Main.hs
@@ -0,0 +1,28 @@
+module Main where
+
+--import qualified System.IO
+--import qualified Control.Monad
+--import qualified Text.Read
+
+--myMap :: (a -> b) -> [a] -> [b]
+--myMap _ []     = []
+--myMap f (x:xs) = f x : map f xs
+
+parseInt :: String -> Int
+parseInt s =
+  case s of
+    ('+':xs) -> read xs
+    _        -> read s
+
+calcFrequency :: Num a => a -> [a] -> a
+calcFrequency x (n:ns) =
+  case ns of
+    [] -> x+n
+    _  -> calcFrequency (x+n) ns
+
+main :: IO ()
+main = do
+  inputs <- readFile "list.txt"
+  let freqs = map parseInt . words $ inputs
+  putStrLn $ "Total frequencies: " ++ (show . length) freqs
+  putStrLn $ "Result frequency: " ++ show (calcFrequency 0 freqs)
